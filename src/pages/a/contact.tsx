dec37bccf2974b40ba28161c88c0f418c2fdc9a8
Clark Tolosa <clarktolosa@gmail.com>
2023-01-20 14:06:48 +0800

    contact page layout

--- src/pages/a/contact.tsx
+++ src/pages/a/contact.tsx
@@ -1,8 +1,59 @@
 import type { NextPage } from "next";
 import Admin from "../../components/Admin";
+import Table from "../../components/Table";
+import type { TableHeading } from "../../types";
+import { api } from "../../utils/api";
+import createTRPCSSG from "../../utils/createTRPCSSG";
+
+export const getStaticProps = async () => {
+  const ssg = await createTRPCSSG();
+
+  await ssg.contact.getAll.fetch();
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+    revalidate: 1,
+  };
+};
+
+const intent = "Contact";
+
+const tableHeadRows: TableHeading[] = [
+  {
+    text: "Description",
+  },
+  {
+    text: "Type",
+  },
+];
 
 const Contact: NextPage = () => {
-  return <Admin pageTitle="Contact"></Admin>;
+  const { data } = api.contact.getAll.useQuery();
+
+  return (
+    <Admin pageTitle={intent}>
+      <Table>
+        <Table.Head>
+          {tableHeadRows.map(({ text, ...rest }, index) => (
+            <Table.Heading {...rest} key={index}>
+              {text}
+            </Table.Heading>
+          ))}
+        </Table.Head>
+        <Table.Body>
+          {data?.map(({ id, desc, type }) => (
+            <Table.BodyRow key={id}>
+              <Table.Data>{desc}</Table.Data>
+              <Table.Data>{type}</Table.Data>
+            </Table.BodyRow>
+          ))}
+        </Table.Body>
+        <Table.Foot intent={intent} colSpan={tableHeadRows.length + 1} />
+      </Table>
+    </Admin>
+  );
 };
 
 export default Contact;
