b99d6cd288487af8922feda6c3f0290387573239
Clark Tolosa <clarktolosa@gmail.com>
2023-02-01 11:54:16 +0800

    updated seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,7 +14,7 @@ async function main() {
   await prisma.contact.createMany({
     data: [
       {
-        desc: "#54 Bakersfield St., Laguna Bel Air 1, Sta. Rosa, Laguna, 4026, Philippines",
+        desc: "Sta. Rosa, Laguna, 4026, Philippines",
         type: "ADDRESS",
       },
       {
@@ -22,7 +22,7 @@ async function main() {
         type: "EMAIL",
       },
       {
-        desc: "(+63)977 4499 113",
+        desc: "(+63)977-4499-113",
         type: "PHONE",
       },
     ],
@@ -42,113 +42,104 @@ async function main() {
     ],
   });
 
-  const feu = await prisma.organization.create({
+  await prisma.organization.create({
     data: {
       position: "Student",
       name: "FEU - Institute of Technology",
       location: "MNL, PH",
       timeframeFrom: new Date(2018, 7, 1, 0, 0, 0, 0),
+
+      educations: {
+        create: {
+          degree: "B.S. Information Technology - Web and Mobile Application",
+          thesis:
+            "Fit-E: An LMS for Wellness and Recreation Program with Student Health Tracking and Recommender",
+        },
+      },
+
+      leaderships: {
+        create: {
+          leadershipProjects: {
+            createMany: {
+              data: [
+                {
+                  course: "Capstone Project",
+                  name: "Fit-E: An LMS for Wellness and Recreation Program with Student Health Tracking and Recommender",
+                  purpose:
+                    "Analyzed project objectives to be feasible given the constrained timeframe. Coordinated with the beneficiary in integrating sensitive information to the system. Engineered Frontend & Backend in parallel to the UI design mockup. Reviewed documentation in alignment with the project desc and objectives",
+                  otherPositions:
+                    "Lead Frontend & Backend Developer, UI/UX Designer, and Project Documentation",
+                },
+                {
+                  course: "eCommerce with Digital Marketing",
+                  name: "Pet Adoption Management System with Integration of MERN Stack and REST API",
+                  otherPositions:
+                    "Lead Frontend & Backend Developer and UI/UX Designer",
+                },
+              ],
+            },
+          },
+        },
+      },
     },
   });
 
-  const spcc = await prisma.organization.create({
+  await prisma.organization.create({
     data: {
       position: "Student",
       name: "Systems Plus Computer College",
       location: "CAC, PH",
       timeframeFrom: new Date(2006, 5, 1, 0, 0, 0, 0),
       timeframeTo: new Date(2018, 2, 1, 0, 0, 0, 0),
+
+      educations: {
+        create: {
+          awards: "Graduated with High Honors & Best in Programming",
+        },
+      },
     },
   });
 
-  await prisma.education.createMany({
-    data: [
-      {
-        organizationId: feu.id,
-        degree: "B.S. Information Technology - Web and Mobile Application",
-        thesis:
-          "Fit-E: An LMS for Wellness and Recreation Program with Student Health Tracking and Recommender",
-      },
-      {
-        organizationId: spcc.id,
-        awards: "Graduated with High Honors & Best in Programming",
-      },
-    ],
-  });
-
-  const aguora = await prisma.organization.create({
+  await prisma.organization.create({
     data: {
       position: "Web Developer Intern",
       name: "Aguora IT Solutions and Technology Inc.",
       location: "WFH",
       timeframeFrom: new Date(2022, 3, 1, 0, 0, 0, 0),
       timeframeTo: new Date(2022, 10, 1, 0, 0, 0, 0),
+
+      experiences: {
+        create: {
+          experienceDescs: {
+            createMany: {
+              data: [
+                {
+                  desc: "Provide support for existing software revisions.",
+                },
+                {
+                  desc: "Build design systems for page, and components reusability.",
+                },
+                {
+                  desc: "Remodeled Santiago City's website for mobile responsive design.",
+                },
+                {
+                  desc: "Translate & Interpret UI mock designs to fully-functional web pages and applications.",
+                },
+                {
+                  desc: "Bootstrap a design system based on UI mock design, resulting in developing application modules with ease.",
+                },
+                {
+                  desc: "Conduct isolated Quality Assurance tests for every assigned task, resulting in filtered tests for the staging process.",
+                },
+                {
+                  desc: "Engineer & Maintain a Project Management System's Frontend & Backend for Marikina Polytechnic College's Smart Campus.",
+                },
+              ],
+            },
+          },
+        },
+      },
     },
-  });
-
-  const aguoraExp = await prisma.experience.create({
-    data: {
-      organizationId: aguora.id,
-    },
-  });
-
-  await prisma.experienceDesc.createMany({
-    data: [
-      {
-        experienceId: aguoraExp.id,
-        desc: "Provide support for existing software revisions.",
-      },
-      {
-        experienceId: aguoraExp.id,
-        desc: "Build design systems for page, and components reusability.",
-      },
-      {
-        experienceId: aguoraExp.id,
-        desc: "Remodeled Santiago City's website for mobile responsive design.",
-      },
-      {
-        experienceId: aguoraExp.id,
-        desc: "Translate & Interpret UI mock designs to fully-functional web pages and applications.",
-      },
-      {
-        experienceId: aguoraExp.id,
-        desc: "Bootstrap a design system based on UI mock design, resulting in developing application modules with ease.",
-      },
-      {
-        experienceId: aguoraExp.id,
-        desc: "Conduct isolated Quality Assurance tests for every assigned task, resulting in filtered tests for the staging process.",
-      },
-      {
-        experienceId: aguoraExp.id,
-        desc: "Engineer & Maintain a Project Management System's Frontend & Backend for Marikina Polytechnic College's Smart Campus.",
-      },
-    ],
-  });
-
-  const feuLead = await prisma.leadership.create({
-    data: {
-      organizationId: feu.id,
-    },
-  });
-
-  await prisma.leadershipProject.createMany({
-    data: [
-      {
-        leadershipId: feuLead.id,
-        course: "Capstone Project",
-        name: "Fit-E: An LMS for Wellness and Recreation Program with Student Health Tracking and Recommender",
-        purpose:
-          "Analyzed project objectives to be feasible given the constrained timeframe. Coordinated with the beneficiary in integrating sensitive information to the system. Engineered Frontend & Backend in parallel to the UI design mockup. Reviewed documentation in alignment with the project desc and objectives",
-        otherPositions:
-          "Lead Frontend & Backend Developer, UI/UX Designer, and Project Documentation",
-      },
-      {
-        leadershipId: feuLead.id,
-        course: "eCommerce with Digital Marketing",
-        name: "Pet Adoption Management System with Integration of MERN Stack and REST API",
-        otherPositions: "Lead Frontend & Backend Developer and UI/UX Designer",
-      },
-    ],
   });
 
   await prisma.miscellaneous.createMany({
@@ -206,6 +197,10 @@ async function main() {
         type: "TECHNICAL",
       },
       {
+        name: "Git",
+        type: "TECHNICAL",
+      },
+      {
         name: "Filipino",
         type: "LANGUAGE",
       },
@@ -222,11 +217,11 @@ async function main() {
         type: "INTEREST",
       },
       {
-        name: "Basketball",
+        name: "PC and Console Gaming",
         type: "INTEREST",
       },
       {
-        name: "PC and Console Gaming",
+        name: "Basketball",
         type: "INTEREST",
       },
     ],
